ea55ce145d9e5eb44d333ebee95a1ea69710a833
BitCryptic <gary@bitcryptic.com>
2026-04-22 22:29:43 +1000

    Add glob loader to blog collection configuration

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '*.md', base: './src/content/blog', ignore: ['_*', 'README*', 'LICENSE*'] }),
   schema: z.object({
     title:       z.string(),
     slug:        z.string(),
