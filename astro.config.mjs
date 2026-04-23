c1b68d4b39efffd23878edea4868b2d8e48b2a93
BitCryptic <gary@bitcryptic.com>
2026-04-23 11:37:24 +1000

    Remove Tailwind integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-  integrations: [tailwind()],
   site: 'https://blog.bitcryptic.com',
 });
